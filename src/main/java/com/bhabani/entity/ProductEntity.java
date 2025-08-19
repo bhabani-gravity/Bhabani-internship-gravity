ae1b46074c7c71ffa111e2b2599ffa3f0b87b7f2
Bhabani Sankar Padhi <bhabanipadhi42@gmail.com>
2025-08-19 15:18:51 +0530

    Added DB integration

--- src/main/java/com/bhabani/entity/ProductEntity.java
+++ src/main/java/com/bhabani/entity/ProductEntity.java
@@ -1,11 +1,17 @@
 package com.bhabani.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class ProductEntity {
+	@Id
 	private Integer productId;
 	private String productName;
 	private Integer productPrice;
